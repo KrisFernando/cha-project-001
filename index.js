e9c75514c039fa08eab87f593d61a48e184ac70e
Kris Fernando <114943416+KrisFernando@users.noreply.github.com>
2022-10-18 20:51:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ exports.handler = async (event, context) => {
   };
   
   try {
-    //let requestBody = event.body; //event.body as json object for test within Lambda
-    let requestBody = JSON.parse(event.body); //event.body as string for API Gateway
+    let requestBody = event.body; //event.body as json object for test within Lambda
+    //let requestBody = JSON.parse(event.body); //event.body as string for API Gateway
     let newID = crypto.createHash('md5').update(requestBody.email).digest('hex');
     await dynamo
           .put({
